e2bca4c496d498d47079b563b98e3173bcfff2ca
pravee42 <darkparadise877@gmail.com>
2021-11-26 08:38:03 +0530

    updated share page

--- src/cards/newscard.jsx
+++ src/cards/newscard.jsx
@@ -55,139 +55,139 @@ export default function NewsCardComponent(props) {
     await toast.success(`Link Copied to clipboard`);
     await console.log(id, "id");
     await navigator.clipboard.writeText(
-      `${window.location.host}/share/${window.btoa(id)}`
+      `https://${window.location.host}/share/${window.btoa(id)}`
     );
-  };
 
-  const closemodel = async () => {
-    await localStorage.setItem("user", "loginlater");
-    await localStorage.setItem(
-      "avatar",
-      "https://e7.pngegg.com/pngimages/929/428/png-clipart-responsive-web-design-navigation-bar-computer-icons-menu-hamburger-button-menu-text-cafe.png"
-    );
-    await localStorage.setItem("name", "User");
-    await sessionStorage.removeItem("news");
-  };
+    const closemodel = async () => {
+      await localStorage.setItem("user", "loginlater");
+      await localStorage.setItem(
+        "avatar",
+        "https://e7.pngegg.com/pngimages/929/428/png-clipart-responsive-web-design-navigation-bar-computer-icons-menu-hamburger-button-menu-text-cafe.png"
+      );
+      await localStorage.setItem("name", "User");
+      await sessionStorage.removeItem("news");
+    };
 
-  // useEffect(async () => {
-  // 	await sessionStorage.setItem("Twitter", `https://twitter.com/intent/tweet?text=${window.location.host}/share/${window.btoa(id)}`)
-  // 	await sessionStorage.setItem("FACEBOOK", `https://web.whatsapp.com://send?text=${window.location.host}/share/${window.btoa(id)}`)
-  // 	await sessionStorage.setItem("Whatsapp", `https://www.facebook.com/sharer/sharer.php?u=${window.location.host}/share/${window.btoa(id)}`)
-  // }, [change])
+    // useEffect(async () => {
+    // 	await sessionStorage.setItem("Twitter", `https://twitter.com/intent/tweet?text=${window.location.host}/share/${window.btoa(id)}`)
+    // 	await sessionStorage.setItem("FACEBOOK", `https://web.whatsapp.com://send?text=${window.location.host}/share/${window.btoa(id)}`)
+    // 	await sessionStorage.setItem("Whatsapp", `https://www.facebook.com/sharer/sharer.php?u=${window.location.host}/share/${window.btoa(id)}`)
+    // }, [change])
 
-  return (
-    <>
-      <div className="newsCard border border-light align-left shadow-sm p-3 mb-5 bg-body rounded ">
-        <img
-          src={props.image}
-          lazy
-          alt={"loading error"}
-          style={{ maxHeight: 100, width: 150 }}
-          className="img-thumbnail"
-        />
-        <div className="news">
-          {props.shortnews && <h6 className="heading ">{props.news}</h6>}
-          {!props.shortnews && <h6 className="heading">{props.news}</h6>}
-          <p className="summary text-muted">{props.shortnews}</p>
-          <div class="align-center" style={{ width: "100%" }}>
-            <a
-              className="btn btn-outline-primary float-start"
-              rel="noreferrer"
-              href={props.link}
-              target="_blank"
-            >
-              Source
-            </a>
-            {localStorage.getItem("user") === "loginlater" && (
+    return (
+      <>
+        <div className="newsCard border border-light align-left shadow-sm p-3 mb-5 bg-body rounded ">
+          <img
+            src={props.image}
+            lazy
+            alt={"loading error"}
+            style={{ maxHeight: 100, width: 150 }}
+            className="img-thumbnail"
+          />
+          <div className="news">
+            {props.shortnews && <h6 className="heading ">{props.news}</h6>}
+            {!props.shortnews && <h6 className="heading">{props.news}</h6>}
+            <p className="summary text-muted">{props.shortnews}</p>
+            <div class="align-center" style={{ width: "100%" }}>
+              <a
+                className="btn btn-outline-primary float-start"
+                rel="noreferrer"
+                href={props.link}
+                target="_blank"
+              >
+                Source
+              </a>
+              {localStorage.getItem("user") === "loginlater" && (
+                <svg
+                  onClick={() =>
+                    add_to_bookmark(props.news, props.link, props.image)
+                  }
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="35"
+                  data-bs-toggle="modal"
+                  data-bs-target="#staticBackdrop"
+                  fill="#0d6efd"
+                  class="bi bi-bookmark-plus-fill float-end"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm6.5-11a.5.5 0 0 0-1 0V6H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V7H10a.5.5 0 0 0 0-1H8.5V4.5z"
+                  />
+                </svg>
+              )}
+              {localStorage.getItem("user") !== "loginlater" && (
+                <svg
+                  onClick={() =>
+                    add_to_bookmark(props.news, props.link, props.image)
+                  }
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="35"
+                  fill="#0d6efd"
+                  class="bi bi-bookmark-plus-fill float-end"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm6.5-11a.5.5 0 0 0-1 0V6H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V7H10a.5.5 0 0 0 0-1H8.5V4.5z"
+                  />
+                </svg>
+              )}
               <svg
-                onClick={() =>
-                  add_to_bookmark(props.news, props.link, props.image)
-                }
+                onClick={() => sharenews(props.news, props.link, props.image)}
                 xmlns="http://www.w3.org/2000/svg"
-                width="30"
+                style={{ marginRight: 20 }}
+                width="20"
                 height="35"
-                data-bs-toggle="modal"
-                data-bs-target="#staticBackdrop"
                 fill="#0d6efd"
-                class="bi bi-bookmark-plus-fill float-end"
+                class="bi bi-share-fill float-end"
                 viewBox="0 0 16 16"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm6.5-11a.5.5 0 0 0-1 0V6H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V7H10a.5.5 0 0 0 0-1H8.5V4.5z"
-                />
+                <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
               </svg>
-            )}
-            {localStorage.getItem("user") !== "loginlater" && (
-              <svg
-                onClick={() =>
-                  add_to_bookmark(props.news, props.link, props.image)
-                }
-                xmlns="http://www.w3.org/2000/svg"
-                width="30"
-                height="35"
-                fill="#0d6efd"
-                class="bi bi-bookmark-plus-fill float-end"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm6.5-11a.5.5 0 0 0-1 0V6H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V7H10a.5.5 0 0 0 0-1H8.5V4.5z"
-                />
-              </svg>
-            )}
-            <svg
-              onClick={() => sharenews(props.news, props.link, props.image)}
-              xmlns="http://www.w3.org/2000/svg"
-              style={{ marginRight: 20 }}
-              width="20"
-              height="35"
-              fill="#0d6efd"
-              class="bi bi-share-fill float-end"
-              viewBox="0 0 16 16"
+            </div>
+            <div
+              class="modal fade"
+              id="staticBackdrop"
+              data-bs-backdrop="static"
+              data-bs-keyboard="false"
+              tabindex="-1"
+              aria-labelledby="staticBackdropLabel"
+              aria-hidden="true"
             >
-              <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
-            </svg>
-          </div>
-          <div
-            class="modal fade"
-            id="staticBackdrop"
-            data-bs-backdrop="static"
-            data-bs-keyboard="false"
-            tabindex="-1"
-            aria-labelledby="staticBackdropLabel"
-            aria-hidden="true"
-          >
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="staticBackdropLabel">
-                    Login to add news to bookmark
-                  </h5>
-                  <button
-                    type="button"
-                    class="btn-close"
-                    data-bs-dismiss="modal"
-                    onClick={closemodel}
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <GoogleAuth clintId={VITE_GAUTH_ID} />{" "}
-                <div class="modal-footer">
-                  <button
-                    type="button"
-                    class="btn btn-danger"
-                    data-bs-dismiss="modal"
-                    onClick={closemodel}
-                  >
-                    Close
-                  </button>
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">
+                      Login to add news to bookmark
+                    </h5>
+                    <button
+                      type="button"
+                      class="btn-close"
+                      data-bs-dismiss="modal"
+                      onClick={closemodel}
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <GoogleAuth clintId={VITE_GAUTH_ID} />{" "}
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-danger"
+                      data-bs-dismiss="modal"
+                      onClick={closemodel}
+                    >
+                      Close
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  };
 }
--- src/cards/share_news_card.jsx
+++ src/cards/share_news_card.jsx
@@ -11,6 +11,11 @@ export default function SharedNewsCardComponent(props) {
       </div>
     );
   };
+
+  useEffect(() => {
+    document.title = props.news;
+  }, []);
+
   const add_to_bookmark = async (news, source, image) => {
     console.log(news, source, image);
     let email = localStorage.getItem("user");
@@ -34,38 +39,43 @@ export default function SharedNewsCardComponent(props) {
     }
   };
   return (
-    <div className="newsCard border border-light shadow-sm p-3 mb-5 bg-body rounded">
-      <img
-        src={props.image}
-        lazy
-        alt={"Loading error"}
-        style={{ maxHeight: 100, width: 150 }}
-        className="img-thumbnail"
-      />
-      <div className="news">
-        {props.shortnews && <h6 className="heading ">{props.news}</h6>}
-        {!props.shortnews && <h6 className="heading">{props.news}</h6>}
-        <p className="summary text-muted">{props.shortnews}</p>
-        <div
-          className="d-flex justify-content-start d-grid gap-3"
-          style={{ width: "50%" }}
-        >
-          <a
-            className="btn btn-outline-primary"
-            rel="noreferrer"
-            href={props.source}
-            target="_blank"
+    <>
+      <link rel="icon" href={props.image} />
+      <div className="newsCard border border-light shadow-sm p-3 mb-5 bg-body rounded">
+        <img
+          src={props.image}
+          lazy
+          alt={"Loading error"}
+          style={{ maxHeight: 100, width: 150 }}
+          className="img-thumbnail"
+        />
+        <div className="news">
+          {props.shortnews && <h6 className="heading ">{props.news}</h6>}
+          {!props.shortnews && <h6 className="heading">{props.news}</h6>}
+          <p className="summary text-muted">{props.shortnews}</p>
+          <div
+            className="d-flex justify-content-start d-grid gap-3"
+            style={{ width: "50%" }}
           >
-            Source
-          </a>
-          <button
-            onClick={() => add_to_bookmark(props.news, props.link, props.image)}
-            className="btn btn-outline-primary space"
-          >
-            Add to Bookmark
-          </button>
+            <a
+              className="btn btn-outline-primary"
+              rel="noreferrer"
+              href={props.source}
+              target="_blank"
+            >
+              Source
+            </a>
+            <button
+              onClick={() =>
+                add_to_bookmark(props.news, props.link, props.image)
+              }
+              className="btn btn-outline-primary space"
+            >
+              Add to Bookmark
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
