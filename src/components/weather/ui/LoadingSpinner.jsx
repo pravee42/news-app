589643118e8be45f17cc4890f3c355bb5da4c8de
ABIPRAVI <65100038+pravee42@users.noreply.github.com>
2021-12-20 09:09:05 +0530

    Revert "Weather forecast component"

--- src/components/weather/ui/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./LoadingSpinner.module.css";
-
-const LoadingSpinner = (props) => {
-  return <div className={styles.spinner} />;
-};
-
-export default LoadingSpinner;
