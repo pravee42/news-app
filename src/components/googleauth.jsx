5e887a11f412c7716caa976246a1aef001a0ddf6
pravee42 <darkparadise877@gmail.com>
2021-12-29 12:31:56 +0530

    updated ligon component

--- src/components/googleauth.jsx
+++ src/components/googleauth.jsx
@@ -85,47 +85,138 @@ export default function GoogleAuth(props) {
 					<div
 						style={{
 							display: "flex",
-							flexDirection: "column",
-							height: "100vh",
-							width: "100%",
-							justifyContent: "center",
-							alignItems: "center",
-							overflowX: "hidden",
+							flexDirection: "row",
+							justifyContent: "space-between",
 						}}
 					>
-						<GoogleLogin
-							clientId={props.authid}
-							onSuccess={Register}
-							theme="dark"
-							icon="false"
-							className="btn btn-outline-primary"
-							buttonText="Register With Google Account"
-							onFailure={Register}
-						/>
-
-						<h4
+						<div
 							style={{
-								margin: 10,
+								display: "flex",
+								flexDirection: "column",
+								height: "100vh",
+								width: "100%",
+								justifyContent: "center",
+								alignItems: "center",
+								overflowX: "hidden",
 							}}
 						>
-							You Have Not Logged In please login with your google
-							account here
-						</h4>
+							<h4
+								style={{
+									margin: 10,
+								}}
+								className="alert alert-danger"
+							>
+								You Have Not Logged In please login with your
+								google account here
+							</h4>
 
-						<GoogleLogin
-							clientId={props.authid}
-							onSuccess={responseGoogle}
-							theme="dark"
-							icon="false"
-							onFailure={responseGoogle}
-						/>
-						<button
-							className="btn btn-outline-primary"
-							style={{ marginTop: 20 }}
-							onClick={loginlater}
+							<GoogleLogin
+								clientId={props.authid}
+								onSuccess={Register}
+								render={(renderProps) => (
+									<button
+										style={{
+											marginTop: 20,
+										}}
+										className="btn btn-outline-primary"
+										onClick={renderProps.onClick}
+										disabled={renderProps.disabled}
+									>
+										Register to News App
+									</button>
+								)}
+								theme="dark"
+								icon="false"
+								className="btn btn-outline-primary"
+								buttonText="Register With Google Account"
+								onFailure={Register}
+							/>
+
+							<GoogleLogin
+								clientId={props.authid}
+								onSuccess={responseGoogle}
+								render={(renderProps) => (
+									<button
+										style={{
+											marginTop: 20,
+										}}
+										className="btn btn-outline-primary"
+										onClick={renderProps.onClick}
+										disabled={renderProps.disabled}
+									>
+										Login 😜
+									</button>
+								)}
+								theme="dark"
+								icon="false"
+								onFailure={responseGoogle}
+							/>
+							<button
+								className="btn btn-outline-primary"
+								style={{ marginTop: 20 }}
+								onClick={loginlater}
+							>
+								Continue Without Login
+							</button>
+						</div>
+						<div
+							className="d-grid gap-3"
+							style={{
+								margin: 20,
+								height: "100%",
+								width: "50%",
+								display: "flex",
+								flexDirection: "column",
+								justifyContent: "space-evenly",
+							}}
 						>
-							Continue Without Login
-						</button>
+							<div class="form-floating mb-3">
+								<input
+									type="email"
+									class="form-control"
+									id="floatingInput"
+									placeholder="name@example.com"
+								/>
+								<label for="floatingInput">Email address</label>
+							</div>
+							<div class="form-floating mb-3">
+								<input
+									type="text"
+									class="form-control"
+									id="floatingInput"
+									placeholder="user name"
+								/>
+								<label for="floatingInput">Name</label>
+							</div>
+							<div class="form-floating mb-3">
+								<input
+									type="password"
+									class="form-control"
+									id="floatingInput"
+									placeholder="password"
+								/>
+								<label for="floatingInput">Password</label>
+							</div>
+							<div class="form-floating mb-3">
+								<input
+									type="password"
+									class="form-control"
+									id="floatingInput"
+									placeholder="password"
+								/>
+								<label for="floatingInput">Password</label>
+							</div>
+							<div class="form-floating mb-3">
+								<input
+									type="url"
+									class="form-control"
+									id="floatingInput"
+									placeholder="https:www.avatar.com/useravatar"
+								/>
+								<label for="floatingInput">Avatar Image</label>
+							</div>
+							<button>Submit</button>
+						</div>
 					</div>
 				) : (
 					<div
@@ -137,7 +228,10 @@ export default function GoogleAuth(props) {
 							padding: "100px",
 						}}
 					>
-						<div className="spinner-border text-primary" role="status">
+						<div
+							className="spinner-border text-primary"
+							role="status"
+						>
 							<span className="visually-hidden">Loading...</span>
 						</div>
 					</div>
